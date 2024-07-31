07cfcd2d137809fedd00e46f9739a7cfe16f0552
HaeChan0305 <kim.haechan@kaist.ac.kr>
2024-07-31 02:23:03 +0000

    results/output_qwen_4_0

--- LIFT/evaluate_pollen.sh
+++ LIFT/evaluate_pollen.sh
@@ -1,10 +1,12 @@
-echo "checkpoint-0"
+# echo "checkpoint-0"
 # mkdir /workspace/output_qwen_4_0/checkpoint-0
-python evaluate_pollen.py -o /workspace/output_qwen_4_0/checkpoint-0/result.json -b 128
+# python evaluate_pollen.py -o /workspace/output_qwen_4_0/checkpoint-0/result.json -b 128
 
 STEP=400
-for i in {1..15}
+for i in {0..15}
 do
-    echo "checkpoint-$((STEP * i))"
-    python evaluate_pollen.py -c /workspace/output_qwen_4_0/checkpoint-$((STEP * i)) -o /workspace/output_qwen_4_0/checkpoint-$((STEP * i))/result.json -b 128
+    # echo "checkpoint-$((STEP * i))"
+    # python evaluate_pollen.py -c /workspace/output_qwen_4_0/checkpoint-$((STEP * i)) -o /workspace/output_qwen_4_0/checkpoint-$((STEP * i))/result.json -b 128
+    mkdir ./results/output_qwen_4_0/checkpoint-$((STEP * i))
+    cp /workspace/output_qwen_4_0/checkpoint-$((STEP * i))/result.json ./results/output_qwen_4_0/checkpoint-$((STEP * i))/result.json
 done
