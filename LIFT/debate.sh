0bae53ecb0f1e1efeff229a7e71a6c0964b0b096
HaeChan0305 <kim.haechan@kaist.ac.kr>
2024-07-31 02:32:36 +0000

    results/output_qwen_4_1

--- LIFT/debate.sh
+++ LIFT/debate.sh
@@ -1,4 +1,4 @@
-STEP=400
+STEP=200
 for i in {1..15}
 do
     # echo "checkpoint-$((STEP * i))"
@@ -6,6 +6,6 @@ do
     # python processing_test_debate.py -c $((STEP * i)) -c1 2 -c2 1 -r 1
     # python debate.py -c /workspace/output_qwen_4_1/checkpoint-$((STEP * i)) -b 8 -r 1
     # python debate.py -c /workspace/output_qwen_4_2/checkpoint-$((STEP * i)) -b 8 -r 1
-    python compute_metric_pollen.py -c1 /workspace/output_qwen_4_0/checkpoint-$((STEP * i))/result.json
+    python compute_metric_pollen.py -c1 ./results/output_qwen_4_1/checkpoint-$((STEP * i))/result.json
 done
 
--- LIFT/evaluate_pollen.sh
+++ LIFT/evaluate_pollen.sh
@@ -2,11 +2,11 @@
 # mkdir /workspace/output_qwen_4_0/checkpoint-0
 # python evaluate_pollen.py -o /workspace/output_qwen_4_0/checkpoint-0/result.json -b 128
 
-STEP=400
+STEP=200
 for i in {0..15}
 do
     # echo "checkpoint-$((STEP * i))"
     # python evaluate_pollen.py -c /workspace/output_qwen_4_0/checkpoint-$((STEP * i)) -o /workspace/output_qwen_4_0/checkpoint-$((STEP * i))/result.json -b 128
-    mkdir ./results/output_qwen_4_0/checkpoint-$((STEP * i))
-    cp /workspace/output_qwen_4_0/checkpoint-$((STEP * i))/result.json ./results/output_qwen_4_0/checkpoint-$((STEP * i))/result.json
+    mkdir ./results/output_qwen_4_1/checkpoint-$((STEP * i))
+    cp /workspace/output_qwen_4_1/checkpoint-$((STEP * i))/result.json ./results/output_qwen_4_1/checkpoint-$((STEP * i))/result.json
 done
